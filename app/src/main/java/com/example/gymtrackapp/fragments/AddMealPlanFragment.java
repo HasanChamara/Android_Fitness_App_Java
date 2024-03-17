5e579728e348d3677b0a4361db63b1789924c9ae
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-03-17 22:50:10 +0530

    ADD MEAL PLAN

--- app/src/main/java/com/example/gymtrackapp/fragments/AddMealPlanFragment.java
+++ app/src/main/java/com/example/gymtrackapp/fragments/AddMealPlanFragment.java
@@ -2,13 +2,21 @@ package com.example.gymtrackapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gymtrackapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,12 +34,19 @@ public class AddMealPlanFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private String userId;
+    private FirebaseFirestore db;
+
+    private TextView weightTextView;
+    private TextView goalTextView;
+
     public AddMealPlanFragment(String userId) {
         this.userId = userId;
     }
+
     public static AddMealPlanFragment newInstance(String userId) {
         return new AddMealPlanFragment(userId);
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +59,45 @@ public class AddMealPlanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_meal_plan, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add_meal_plan, container, false);
+        db = FirebaseFirestore.getInstance();
+
+        weightTextView = rootView.findViewById(R.id.weightTextView);
+        goalTextView = rootView.findViewById(R.id.goalTextView);
+
+        fetchAndDisplayBiometricDetails();
+
+
+        return rootView;
+
     }
+    private void fetchAndDisplayBiometricDetails() {
+        db.collection("biometricDetails")
+                .whereEqualTo("userID", userId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+
+                                Object weightObject = document.get("weight");
+                                    Float weight = ((Double) weightObject).floatValue();
+                                    weightTextView.setText(String.valueOf(weight));
+
+                                Object goalObject = document.get("goal");
+                                    String goal = (String) goalObject;
+                                    goalTextView.setText(goal);
+
+
+                            }
+                        } else {
+                            Toast.makeText(requireContext(), "Error getting documents", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
 }

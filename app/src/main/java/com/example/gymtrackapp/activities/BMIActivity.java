fd78af55cfed6944482ee12f60f56bab50a70bfe
HasanChamara <96457923+HasanChamara@users.noreply.github.com>
2024-03-18 10:02:31 +0530

    Updated BMI Activity

--- app/src/main/java/com/example/gymtrackapp/activities/BMIActivity.java
+++ app/src/main/java/com/example/gymtrackapp/activities/BMIActivity.java
@@ -1,6 +1,9 @@
 package com.example.gymtrackapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gymtrackapp.R;
+import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -17,9 +21,18 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+import java.text.DecimalFormat;
+
 public class BMIActivity extends AppCompatActivity {
 
     TextView resultTextView;
+    TextView bmiValue;
+    TextView categoryTextView;
+    TextView importantTextView;
+    TextView messageTextView;
+    MaterialCardView bmiCardView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +40,11 @@ public class BMIActivity extends AppCompatActivity {
 
 
         resultTextView = findViewById(R.id.resultTextView);
+        bmiValue = findViewById(R.id.bmiValue);
+        categoryTextView = findViewById(R.id.categoryTextView);
+        importantTextView = findViewById(R.id.importentTextView);
+        messageTextView = findViewById(R.id.messageTextView);
+        bmiCardView = findViewById(R.id.bmiCardView);
 
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -74,17 +92,51 @@ public class BMIActivity extends AppCompatActivity {
 
     private void displayBMIResult(float bmi) {
         String bmiCategory;
+        int textColor = 0;
+        String bmiMessage;
+        String important;
+        int borderColor = 0;
+
         if (bmi < 18.5) {
             bmiCategory = "Underweight";
+            textColor = ContextCompat.getColor(this, R.color.yellow_color);
+            important = "Important";
+            bmiMessage = "You are underweight. Please consider consulting a healthcare professional.";
+            borderColor = ContextCompat.getColor(this, R.color.yellow_color);
+
         } else if (bmi < 25) {
-            bmiCategory = "Normal weight";
+            bmiCategory = "Normal";
+            textColor = ContextCompat.getColor(this, R.color.green_color);
+            important = "Important";
+            bmiMessage = "Congratulations! You have a normal BMI. Keep up the good work!";
+            borderColor = ContextCompat.getColor(this, R.color.green_color);
+
         } else if (bmi < 30) {
             bmiCategory = "Overweight";
+            textColor = ContextCompat.getColor(this, R.color.orange_red_color);
+            important = "Important";
+            bmiMessage = "You are overweight. Consider making healthy lifestyle changes.";
+            borderColor = ContextCompat.getColor(this, R.color.orange_red_color);
+
         } else {
             bmiCategory = "Obese";
+            textColor = ContextCompat.getColor(this, R.color.cadmium_red_color);
+            important = "Important";
+            bmiMessage = "You are obese. It's important to prioritize your health. Please consult a healthcare professional.";
+            borderColor = ContextCompat.getColor(this, R.color.cadmium_red_color);
         }
 
-        String result = "BMI: " + String.format("%.2f", bmi) + "\nCategory: " + bmiCategory;
-        resultTextView.setText(result);
+        float floatValue = bmi; // Example float value
+        DecimalFormat decimalFormat = new DecimalFormat("#.#"); // Define the format
+        String formattedBmiValue = decimalFormat.format(floatValue);
+        bmiValue.setText(formattedBmiValue);
+        bmiValue.setTextColor(textColor);
+        categoryTextView.setText(bmiCategory);
+        categoryTextView.setTextColor(textColor);
+        importantTextView.setText(important);
+        importantTextView.setTextColor(textColor);
+        messageTextView.setText(bmiMessage);
+        messageTextView.setTextColor(textColor);
+        bmiCardView.setStrokeColor(borderColor);
     }
 }
